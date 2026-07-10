9d70401d8b3c946538b8d1a3b1fd0b11d12b7591
MYQ <jerry200223200223@126.com>
2026-07-10 08:00:55 +0800

    chore: add shadcn UI components (popover, command, dialog, tooltip) and update dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { GlobalWorkerOptions } from 'pdfjs-dist'
 import App from './App.tsx'
-import './index.css'
+// PDF.js official viewer CSS (provides textLayer positioning etc.)
+import 'pdfjs-dist/web/pdf_viewer.css'
+// react-pdf-highlighter-plus styles (overlays on top of pdfjs)
 import 'react-pdf-highlighter-plus/style/style.css'
 import 'react-pdf-highlighter-plus/style/pdf_viewer.css'
+// Our overrides (last to win)
+import './index.css'
 
 // Set PDF.js worker source
 const pdfVersion = '4.10.38'
