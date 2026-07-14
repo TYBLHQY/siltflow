1150644ed80291461d29a156e04893fae5e401bb
MYQ <jerry200223200223@126.com>
2026-07-15 00:15:58 +0800

    chore(mobile): clean up navigation types - remove Review tab and stack types

--- apps/mobile/src/navigation/types.ts
+++ apps/mobile/src/navigation/types.ts
@@ -1,39 +1,11 @@
-import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 import type { BottomTabScreenProps } from "@react-navigation/bottom-tabs";
-import type { CompositeScreenProps, NavigatorScreenParams } from "@react-navigation/native";
-
-// ---------------------------------------------------------------------------
-// Tab Navigator
-// ---------------------------------------------------------------------------
 
 export type TabParamList = {
-  DocumentsTab: NavigatorScreenParams<DocumentsStackParamList>;
-  ReviewTab: undefined;
+  DocumentsTab: undefined;
   StatsTab: undefined;
   SettingsTab: undefined;
 };
 
-// ---------------------------------------------------------------------------
-// Documents Stack
-// ---------------------------------------------------------------------------
-
-export type DocumentsStackParamList = {
-  DocumentList: undefined;
-  DocumentDetail: { documentId: string; title: string };
-};
-
-// ---------------------------------------------------------------------------
-// Screen prop types
-// ---------------------------------------------------------------------------
-
-export type DocumentsScreenProps = CompositeScreenProps<
-  BottomTabScreenProps<TabParamList, "DocumentsTab">,
-  NativeStackScreenProps<DocumentsStackParamList>
->;
-
-export type ReviewTabScreenProps = BottomTabScreenProps<TabParamList, "ReviewTab">;
+export type DocumentsScreenProps = BottomTabScreenProps<TabParamList, "DocumentsTab">;
 export type StatsTabScreenProps = BottomTabScreenProps<TabParamList, "StatsTab">;
 export type SettingsTabScreenProps = BottomTabScreenProps<TabParamList, "SettingsTab">;
-
-export type DocumentListScreenProps = NativeStackScreenProps<DocumentsStackParamList, "DocumentList">;
-export type DocumentDetailScreenProps = NativeStackScreenProps<DocumentsStackParamList, "DocumentDetail">;
